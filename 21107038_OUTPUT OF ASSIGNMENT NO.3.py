e67429232e9e6fc8981cbc431e9a449f11c2723e
MADHAV <104258321+chaudharymadhav@users.noreply.github.com>
2022-05-12 10:31:10 +0530

    Add files via upload

---
+++ 21107038_OUTPUT OF ASSIGNMENT NO.3.py
@@ -0,0 +1,61 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\mkcsn\PycharmProjects\python College Assignement\21107038_ASSIGNMENT NO.3..py
+Enter Your input as decimal number and get output as binary number
+15
+0b1111
+
+This is an interactive Python calculator program.
+Enter the expression only by using these operations (+,-,*,/,**,//,%) -:6**4+8-7*8*9-(6//4)-(8%3)
+your answer is equal to-: 797
+
+Answer of part(a)-: 35
+Enter the value of n
+4
+Answer of part(b)-: 6.0
+Enter the value of r1
+6
+Answer of part(c)-: 452.3893421169302
+Enter the value of r-: 1
+Enter the value of angle a-: 56
+Enter the value of angle b-: 56
+Answer of part(d)-:  1.0
+Enter the value of x1-: 5
+Enter the value of x2-: 6
+Enter the value of y1-: 7
+Enter the value of y2-: 9
+Answer of part(d)-:  y2-y1/x2-x1 =  2.0
+
+In the range(5)
+0
+1
+2
+3
+4
+In the range(3, 10)
+3
+4
+5
+6
+7
+8
+9
+In the range(4 ,13, 3)
+4
+7
+10
+In the range(15, 5, -2)
+15
+13
+11
+9
+7
+In the range(5, 3)
+
+This program computes the molecular weight of carbohydrates 
+,for that Enter the number of atoms of hydrogen,carbon,oxygen 
+Enter the number of hydrogen atoms-: 12
+Enter the number of carbon atoms-: 6
+Enter the number of oxygen atoms-: 6
+The moleculer weight of carbohydrate = weight of hydrogen + weight of carbon + weight of oxygen= 180.15912
